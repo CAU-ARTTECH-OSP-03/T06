55a75b3d15648231f11cd2c13945822bac871d33
oheri0327 <goflsmsrnldudna@naver.com>
2021-12-19 15:17:12 +0900

    Add T06

---
+++ Tamagotchi/Assets/TiltGame/Assets/ExitHoleScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitHoleScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        BallControlScript.setYouWinToTrue();
+    }
+}
